9bb383070d262e4fb95cb11edab3f0094ec3fca0
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2022-11-29 00:37:31 +0200

    📖 DOC: CL update

--- Core/Changelog/6.1.2.lua
+++ Core/Changelog/6.1.2.lua
@@ -3,7 +3,7 @@ local TXUI, F, E, I, V, P, G = unpack(select(2, ...))
 TXUI.Changelog["6.1.2"] = {
   HOTFIX = true,
   CHANGES = {
-    "* General",
-    "one two three",
+    "* Misc",
+    "Plater: Update interrupt cast color mod",
   },
 }
