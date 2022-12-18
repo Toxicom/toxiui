5adf6034588fb9d8a3ea4749f7673c6b4456d1ac
Rochelle Sieben <ryadaproductions@proton.me>
2022-12-18 23:51:47 +0100

    Better solution for fixing the taint

--- Core/Animations.lua
+++ Core/Animations.lua
@@ -995,10 +995,7 @@ end
 Update["move"] = function(self, elapsed, i)
   self.Timer = self.Timer + elapsed
 
-  if UnitAffectingCombat("player") then
-    tremove(Update, i)
-    self.Playing = false
-  elseif self.Timer >= self.Duration then
+  if self.Timer >= self.Duration then
     tremove(Updater, i)
     self.Parent:SetPoint(self.A1, self.P, self.A2, self.EndX, self.EndY)
     self.Playing = false
