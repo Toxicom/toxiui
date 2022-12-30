61fd3f8ec6a1c79ccddacca9f0f3e57d6001c538
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2022-12-30 04:47:37 +0200

    👌 IMPROVE: Update ElvUI Globals
    -   Move World Map coordinates to bottom right;
    -   Increase default elvui options window size.

--- Core/Changelog/6.1.3.lua
+++ Core/Changelog/6.1.3.lua
@@ -5,6 +5,9 @@ TXUI.Changelog["6.1.3"] = {
   CHANGES = {
     "* ToxiUI",
     "Perhaps better handling for those very big UW screens",
+    "* ElvUI",
+    "Move World Map coordinates to bottom right",
+    "Increase default options window size",
     "* Plater",
     "Disable Target color on the M+ Colored Mobs mod",
   },
