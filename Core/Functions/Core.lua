1ab7b59d01e12dbc601d6301a8f820c4f100fbfe
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-01-19 23:02:28 +0200

    🚀 RELEASE: 6.1.4

--- Core/Functions/Core.lua
+++ Core/Functions/Core.lua
@@ -121,6 +121,7 @@ end
 
 function F.PixelPerfect()
   local perfectScale = 768 / E.physicalHeight
+  if E.physicalHeight == 2160 then perfectScale = perfectScale * 2 end
   return perfectScale
 end
 
