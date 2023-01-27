677314d0844ca5cbedeca8b76318bef27fc218a0
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-01-28 01:12:41 +0200

    📖 DOC: Bump TOC

--- Core/Changelog/6.1.5.lua
+++ Core/Changelog/6.1.5.lua
@@ -1,9 +1,9 @@
 local TXUI, F, E, I, V, P, G = unpack(select(2, ...))
 
-TXUI.Changelog["6.1.4"] = {
+TXUI.Changelog["6.1.5"] = {
   HOTFIX = false,
   CHANGES = {
     "* ToxiUI",
-    "Hello",
+    "Update for Patch 10.0.5",
   },
 }
