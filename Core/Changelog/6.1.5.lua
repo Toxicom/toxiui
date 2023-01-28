8d8d24f488d66a22406357e066035eb1a35c3c1e
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-01-28 13:47:33 +0200

    🚀 RELEASE: 6.1.5

---
+++ Core/Changelog/6.1.5.lua
@@ -0,0 +1,10 @@
+local TXUI, F, E, I, V, P, G = unpack(select(2, ...))
+
+TXUI.Changelog["6.1.5"] = {
+  HOTFIX = true,
+  CHANGES = {
+    "* ToxiUI",
+    "Update for Patch 10.0.5",
+    "WunderBar: Temporarily disable MicroMenu's Guild tooltip and right click",
+  },
+}
