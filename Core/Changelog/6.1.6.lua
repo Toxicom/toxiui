593867c72f118dc1c6977c4b7902c3c2e1c9de07
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-02-09 19:41:28 +0200

    📖 DOC: Changelog

--- Core/Changelog/6.1.6.lua
+++ Core/Changelog/6.1.6.lua
@@ -5,5 +5,6 @@ TXUI.Changelog["6.1.6"] = {
   CHANGES = {
     "* ToxiUI",
     "Details: Fix 'Deaths' window LUA error (still needs a fix for class icons). Credits to " .. F.String.Class("shrift", "DEATHKNIGHT"),
+    "Installer: Change installer so it's more in line with other plugins",
   },
 }
