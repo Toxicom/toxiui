a8413acbc309a92e1a44adfa787fa6c451b1a956
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-05-07 01:08:53 +0300

    📦 NEW: Icon texture for AddOns window

--- Core/Changelog/6.2.0.lua
+++ Core/Changelog/6.2.0.lua
@@ -7,5 +7,6 @@ TXUI.Changelog["6.2.0"] = {
     "Fix scaling at 5k resolution. Credits to " .. F.String.Class("Nikipe", "MAGE"),
     "Armory: Update API variable used for weapon item class. Credits to " .. F.String.Beta("Stiimo"),
     "WunderBar: Fix DataText's tooltips by creating virtual frames. Credits to " .. F.String.Beta("Stiimo") .. " & " .. F.String.Class("ElvUI WindTools", "SHAMAN"),
+    "Misc: Add IconTexture to the AddOns window",
   },
 }
