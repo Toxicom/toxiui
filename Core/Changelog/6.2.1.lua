54e2500009a4070dd5a3b5a0b43409fdabdd44cc
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-05-08 23:56:17 +0300

    📖 DOC: Changelog updates

--- Core/Changelog/6.2.1.lua
+++ Core/Changelog/6.2.1.lua
@@ -4,9 +4,13 @@ TXUI.Changelog["6.2.1"] = {
   HOTFIX = true,
   CHANGES = {
     "* ToxiUI",
+    "WunderBar: Fixed Time module by populating its virtual frame with animation-related dummy functions. Credits to " .. F.String.Beta("Stiimo"),
+    "Installer: Changed dialog's information for a better UX",
     "Misc: Add ToxiUI Addon Compartment. Credits to " .. F.String.Class("Knuffelpanda", "MONK"),
-    "WunderBar: Fixed Time module by populating its virtual frame with animation-related dummy functions. Credits to " .. F.String.Class("Stiimo", "PRIEST"),
     "* BigWigs",
     "Add support for 3440x1440",
+    "Add support for 1920x1080 " .. F.String.Error("[DPS Only for now]"),
+    "* DBM",
+    F.String.Error("Removed") .. " Deadly Boss Mods",
   },
 }
