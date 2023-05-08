916ca6915238d5c36539e3f1c75e97db119460c0
Maxim Abramov <maxim.abramov@s-telematics.ru>
2023-05-08 23:19:40 +0400

    FIX: populated time virtual frame with animation-related dummy functions

--- Core/Changelog/6.2.1.lua
+++ Core/Changelog/6.2.1.lua
@@ -5,5 +5,6 @@ TXUI.Changelog["6.2.1"] = {
   CHANGES = {
     "* ToxiUI",
     "Misc: Add ToxiUI Addon Compartment. Credits to " .. F.String.Class("Knuffelpanda", "MONK"),
+    "WunderBar: Fixed Time module by populating its virtual frame with animation-related dummy functions. Credits to " .. F.String.Class("Stiimo", "PRIEST"),
   },
 }
