925bd572ed05cb6769f31617a3d6f71e86d48eb0
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-07-12 21:50:15 +0300

    📖 DOC: Prepare 6.2.3

---
+++ Core/Changelog/6.2.3.lua
@@ -0,0 +1,13 @@
+local TXUI, F, E, I, V, P, G = unpack(select(2, ...))
+
+TXUI.Changelog["6.2.3"] = {
+  HOTFIX = true,
+  CHANGES = {
+    "* Plater",
+    "Update " .. F.String.ToxiUI("M+ Colored Mobs") .. " mod",
+    "Update " .. F.String.ToxiUI("Interrupt not ready Cast Color") .. " mod",
+  },
+  CHANGES_RETAIL = {
+    "Update for " .. F.String.ToxiUI("Fractures in Time") .. " [10.1.5]",
+  },
+}
