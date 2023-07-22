9eff51847b476516c0e8eceb6f6e2c7db16a2ed1
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-07-22 23:04:52 +0300

    📖 DOC: Update old changelogs

--- Core/Changelog/6.1.2.lua
+++ Core/Changelog/6.1.2.lua
@@ -5,11 +5,11 @@ TXUI.Changelog["6.1.2"] = {
   CHANGES = {
     "* ToxiUI",
     "Fix Gradient Mode's Dead unit color",
-    F.String.Good("[NEW] ") .. "Evoker spec icons. Credits to " .. F.String.Class("Nawuko", "MONK"),
+    F.String.Good("NEW: ") .. "Evoker spec icons. Credits to " .. F.String.Class("Nawuko", "MONK"),
     "WunderBar: Add Valdrakken & Tol Barad portals for mages",
     "WunderBar: Change default background",
     "Armory: Update enchant slots for Dragonflight",
-    F.String.Good("[NEW] ") .. "Armory: Socket warning for neck slot. Credits to " .. F.String.Epic("Ryada"),
+    F.String.Good("NEW: ") .. "Armory: Socket warning for neck slot. Credits to " .. F.String.Epic("Ryada"),
     "VehicleBar: Fix taint that would happen during combat while mounting. Credits to " .. F.String.Epic("Ryada"),
 
     "* ElvUI",
