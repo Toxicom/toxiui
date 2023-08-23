d353bafacd908da5aa4b00da4ae83007377ff93c
Toxicom <tomas.98.leo@gmail.com>
2023-08-23 20:05:41 +0300

    📖 DOC: CL update

--- Core/Changelog/6.3.0.lua
+++ Core/Changelog/6.3.0.lua
@@ -7,6 +7,7 @@ TXUI.Changelog["6.3.0"] = {
     F.String.Good("NEW: ") .. "UnitFrame Class Icons",
     F.String.Good("NEW: ") .. "Stylized icons " .. F.String.Class("(icons8.com)", "ROGUE") .. " for Dead and Disconnected state to match UnitFrame class icons",
     F.String.Good("NEW: ") .. "Combat icon for Player UnitFrame " .. F.String.Class("(icons8.com)", "ROGUE"),
+    F.String.Good("NEW: ") .. "UnitFrame style in the installer",
     F.String.RandomClassColor("FadePersist: ") .. "Properly disable the module when ElvUI ActionBars are disabled",
     F.String.RandomClassColor("VehicleBar: ") .. "Properly disable the module when ElvUI ActionBars are disabled",
     F.String.RandomClassColor("Armory: ") .. "Possibly fix a bug with item level coloring based on achievement status",
