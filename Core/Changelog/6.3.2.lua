5bc766e99f504ca6900478d26648708e81b45393
Toxicom <tomas.98.leo@gmail.com>
2023-09-21 22:18:03 +0300

    📖 DOC: lowercase letter

--- Core/Changelog/6.3.2.lua
+++ Core/Changelog/6.3.2.lua
@@ -37,7 +37,7 @@ TXUI.Changelog["6.3.2"] = {
     "* Settings refactoring",
     "Move " .. TXUI.Title .. " Game Menu Button settings to " .. F.String.FastGradientHex("Skins", "#ff77a9", "#b4004e") .. " -> " .. F.String.Class("ElvUI"),
 
-    "* Development Improvements",
+    "* Development improvements",
     "Refactor Additional Scaling feature, it now has it's own separate tab in " .. F.String.FastGradientHex("Miscellaneous", "#b085f5", "#4d2c91"),
     "Make the '" .. F.String.ToxiUI("/tx install") .. "' command actually open the installer",
     "Add alias to open " .. TXUI.Title .. " Installer - '" .. F.String.ToxiUI("/tx i") .. "'",
