931f5df2c5571b87ec15836d1438f75bdea37eef
Toxicom <tomas.98.leo@gmail.com>
2023-10-08 23:50:55 +0300

    🚀 RELEASE: 6.4.2

--- Core/Changelog/6.4.2.lua
+++ Core/Changelog/6.4.2.lua
@@ -6,19 +6,11 @@ TXUI.Changelog["6.4.2"] = {
     "* Breaking changes",
     "Remove WeakAuras skinning feature from " .. TXUI.Title .. F.String.Sublist("WeakAuras v5.7.2 removed skinning support for other AddOns"),
 
-    "* New features",
-
     "* Bug fixes",
     TXUI.Title .. ": Potentially fix Details dark theme gradient name error for Neltharus Weapons etc.",
     TXUI.Title .. ": Fix Mining profession for WunderBar in Classic Era",
 
-    "* Profile updates",
-
     "* Documentation",
     "Increase minimum required " .. F.String.ElvUI() .. " version to 13.42",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
