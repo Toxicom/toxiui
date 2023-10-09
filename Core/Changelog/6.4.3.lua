7617e2c13f41974f42d38b6e2148ce279c71dafb
Rochelle <RyadaProductions@users.noreply.github.com>
2023-10-09 17:48:30 +0200

    🐛 FIX: remove the need to reload the ui when adjusting scaling of UI elements (#72)

--- Core/Changelog/6.4.3.lua
+++ Core/Changelog/6.4.3.lua
@@ -13,6 +13,7 @@ TXUI.Changelog["6.4.3"] = {
 
     "* Bug fixes",
     TXUI.Title .. ": Removed the need to forceload addons when adjusting scale through " .. F.String.ToxiUI("Additional Scaling"),
+    TXUI.Title .. ": Removed the need to reload when adjusting scale through " .. F.String.ToxiUI("Additional Scaling"),
 
     "* Profile updates",
 
