decef7eb49bbf8c33f12efda8a717242e80a4b59
Toxicom <tomas.98.leo@gmail.com>
2023-10-10 00:48:48 +0300

    fuck

---
+++ Modules/Misc/AdditionalScaling/Core.lua
@@ -0,0 +1,35 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+local M = TXUI:GetModule("Misc")
+
+function M:AdditionalScaling()
+  -- Don't init if its not a TXUI profile or requirements are not met
+  if not TXUI:HasRequirements(I.Requirements.AdditionalScaling) then return end
+
+  -- check if database is present
+  if E.db and E.db.TXUI then
+    M:SetElementScale("map", "WorldMapFrame")
+    M:SetElementScale("characterFrame", "CharacterFrame")
+    M:SetElementScale("dressingRoom", "DressUpFrame")
+
+    -- In the next parts, if the AddOn isn't loaded by the game yet,
+    -- we add it to a list to be loaded as soon as the AddOn has been loaded.
+    -- Otherwise we can scale the UI element directly.
+    M:AddCallbackOrScale("Blizzard_InspectUI", self.ScaleInspectUI)
+
+    -- Retail scaling
+    if TXUI.IsRetail then
+      M:AddCallbackOrScale("Blizzard_Collections", self.ScaleCollections)
+      M:AddCallbackOrScale("Blizzard_ClassTalentUI", self.ScaleTalents)
+    end
+
+    -- Wrath & Classic scaling
+    if not TXUI.IsRetail then
+      -- Classic: Talents
+      M:AddCallbackOrScale("Blizzard_TalentUI", self.ScaleTalents)
+    end
+  else
+    TXUI:LogDebug("AdditionalScaling > E.db or E.db.TXUI not found, skipping scaling!")
+  end
+end
+
+M:AddCallback("AdditionalScaling")
