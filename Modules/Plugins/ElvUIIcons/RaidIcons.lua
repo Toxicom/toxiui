325c31c326a03c0bbef60dbdcc3ed9c352f9293b
Toxicom <tomas.98.leo@gmail.com>
2023-10-28 01:18:59 +0300

    👌 IMPROVE: use vars

--- Modules/Plugins/ElvUIIcons/RaidIcons.lua
+++ Modules/Plugins/ElvUIIcons/RaidIcons.lua
@@ -15,21 +15,21 @@ function RAI:ChangeRaidIcon()
     local master = frame.MasterLooterIndicator
     -- local raid = frame.RaidRoleIndicator
 
-    if frame.LeaderIndicator and db.leader.enabled then
+    if leader and db.leader.enabled then
       if db.leader.theme then
         leader:SetTexCoord(0, 1, 0, 1)
         leader:SetTexture(F.GetMedia(I.Media.StateIcons, I.ElvUIIcons.Leader[self.db.leader.theme]))
       end
     end
 
-    if frame.AssistantIndicator and db.assist.enabled then
+    if assistant and db.assist.enabled then
       if db.assist.theme then
         assistant:SetTexCoord(0, 1, 0, 1)
         assistant:SetTexture(F.GetMedia(I.Media.StateIcons, I.ElvUIIcons.Assist[self.db.assist.theme]))
       end
     end
 
-    if frame.MasterLooterIndicator then
+    if master and db.looter.enabled then
       if db.looter.theme then
         master:SetTexCoord(0, 1, 0, 1)
         master:SetTexture(F.GetMedia(I.Media.StateIcons, I.ElvUIIcons.Looter[self.db.theme]))
