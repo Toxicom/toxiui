bf8ee9df1180d59606b20a7eb6b64120e9cf758f
Toxicom <tomas.98.leo@gmail.com>
2023-12-08 19:34:24 +0200

    🐛 FIX: UF styles for healer layout

--- Modules/Styles/UnitFrames.lua
+++ Modules/Styles/UnitFrames.lua
@@ -11,317 +11,350 @@ function ST:BuildUnitFramesProfile()
   end
 
   local pf = E.db
+  local IsHealer = pf.TXUI.installer.layout == I.Enum.Layouts.HEALER
 
   if style == "New" then
     -- UnitFrames
-    F.Table.Crush(pf.unitframe.units, {
-      -- Player
-      player = {
-        height = F.Dpi(30),
+    F.Table.Crush(
+      pf.unitframe.units,
+      {
+        -- Player
+        player = {
+          height = F.Dpi(30),
 
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
-          },
-          ["!Health"] = {
-            text_format = "[tx:health:percent:nosign]",
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
+            },
+            ["!Health"] = {
+              text_format = "[tx:health:percent:nosign]",
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
 
-            font = F.FontOverride(I.Fonts.Primary),
-            size = F.FontSizeScaled(36),
-            fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
+              font = F.FontOverride(I.Fonts.Primary),
+              size = F.FontSizeScaled(36),
+              fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.ChooseForTheme(F.Dpi(5), F.Dpi(-10)),
+              yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+            },
           },
-          ["!ClassIcon"] = {
-            xOffset = F.ChooseForTheme(F.Dpi(5), F.Dpi(-10)),
-            yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+
+          raidRoleIcons = {
+            yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(15)),
           },
         },
 
-        raidRoleIcons = {
-          yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(15)),
+        target = {
+          height = F.Dpi(30),
+
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
+            },
+            ["!Health"] = {
+              text_format = "[tx:health:percent:nosign]",
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
+
+              font = F.FontOverride(I.Fonts.Primary),
+              size = F.FontSizeScaled(36),
+              fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(10)),
+              yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+            },
+            ["!Classification"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(15)),
+            },
+          },
+
+          raidRoleIcons = {
+            yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(15)),
+          },
+        },
+
+        pet = {
+          height = F.Dpi(15),
+
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(10)),
+            },
+          },
+        },
+
+        targettarget = {
+          height = F.Dpi(15),
+
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(10)),
+            },
+          },
+        },
+
+        focus = {
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(20)),
+            },
+            ["!Health"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(20)),
+
+              font = F.FontOverride(I.Fonts.Primary),
+              size = F.FontSizeScaled(36),
+              fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(10)),
+              yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+            },
+          },
+        },
+
+        party = {
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
+            },
+            ["!Health"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
+
+              font = F.FontOverride(I.Fonts.Primary),
+              size = F.FontSizeScaled(36),
+              fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.ChooseForTheme(F.Dpi(5), F.Dpi(-10)),
+              yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+            },
+          },
+
+          raidRoleIcons = {
+            yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(15)),
+          },
+        },
+
+        arena = {
+          customTexts = {
+            ["!Health"] = {
+              text_format = "[tx:health:percent:nosign]",
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
+
+              font = F.FontOverride(I.Fonts.Primary),
+              size = F.FontSizeScaled(36),
+              fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
+            },
+
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
+            },
+          },
+        },
+
+        boss = {
+          customTexts = {
+            ["!Health"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
+
+              font = F.FontOverride(I.Fonts.Primary),
+              size = F.FontSizeScaled(36),
+              fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
+            },
+
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
+            },
+          },
         },
       },
+      F.Table.If(IsHealer, {
+        party = {
+          customTexts = {
+            ["!Health"] = {
+              yOffset = F.Dpi(0),
+            },
 
-      target = {
-        height = F.Dpi(30),
-
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
-          },
-          ["!Health"] = {
-            text_format = "[tx:health:percent:nosign]",
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
-
-            font = F.FontOverride(I.Fonts.Primary),
-            size = F.FontSizeScaled(36),
-            fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
-          },
-          ["!ClassIcon"] = {
-            xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(10)),
-            yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
-          },
-          ["!Classification"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(15)),
+            ["!Name"] = {
+              yOffset = F.Dpi(0),
+            },
           },
         },
-
-        raidRoleIcons = {
-          yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(15)),
-        },
-      },
-
-      pet = {
-        height = F.Dpi(15),
-
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(10)),
-          },
-        },
-      },
-
-      targettarget = {
-        height = F.Dpi(15),
-
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(10)),
-          },
-        },
-      },
-
-      focus = {
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(20)),
-          },
-          ["!Health"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(20)),
-
-            font = F.FontOverride(I.Fonts.Primary),
-            size = F.FontSizeScaled(36),
-            fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
-          },
-          ["!ClassIcon"] = {
-            xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(10)),
-            yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
-          },
-        },
-      },
-
-      party = {
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
-          },
-          ["!Health"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
-
-            font = F.FontOverride(I.Fonts.Primary),
-            size = F.FontSizeScaled(36),
-            fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
-          },
-          ["!ClassIcon"] = {
-            xOffset = F.ChooseForTheme(F.Dpi(5), F.Dpi(-10)),
-            yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
-          },
-        },
-
-        raidRoleIcons = {
-          yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(15)),
-        },
-      },
-
-      arena = {
-        customTexts = {
-          ["!Health"] = {
-            text_format = "[tx:health:percent:nosign]",
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
-
-            font = F.FontOverride(I.Fonts.Primary),
-            size = F.FontSizeScaled(36),
-            fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
-          },
-
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
-          },
-        },
-      },
-
-      boss = {
-        customTexts = {
-          ["!Health"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(15)),
-
-            font = F.FontOverride(I.Fonts.Primary),
-            size = F.FontSizeScaled(36),
-            fontOutline = F.FontStyleOverride(I.Fonts.Primary, "SHADOWOUTLINE"),
-          },
-
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(15)),
-          },
-        },
-      },
-    })
+      })
+    )
   end
 
   if style == "Old" then
     -- UnitFrames
-    F.Table.Crush(pf.unitframe.units, {
-      -- Player
-      player = {
-        height = F.ChooseForTheme(F.Dpi(30), F.Dpi(40)),
+    F.Table.Crush(
+      pf.unitframe.units,
+      {
+        -- Player
+        player = {
+          height = F.ChooseForTheme(F.Dpi(30), F.Dpi(40)),
 
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
-          },
-          ["!Health"] = {
-            text_format = "[tx:health:current:shortvalue] || [tx:health:percent:nosign]",
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
+            },
+            ["!Health"] = {
+              text_format = "[tx:health:current:shortvalue] || [tx:health:percent:nosign]",
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
 
-            font = F.FontOverride("- Steelfish"),
-            size = F.FontSizeScaled(22),
-            fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
+              font = F.FontOverride("- Steelfish"),
+              size = F.FontSizeScaled(22),
+              fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.ChooseForTheme(F.Dpi(5), F.Dpi(-32)),
+              yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+            },
           },
-          ["!ClassIcon"] = {
-            xOffset = F.ChooseForTheme(F.Dpi(5), F.Dpi(-32)),
-            yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+
+          raidRoleIcons = {
+            yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(10)),
           },
         },
 
-        raidRoleIcons = {
-          yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(10)),
+        target = {
+          height = F.ChooseForTheme(F.Dpi(30), F.Dpi(40)),
+
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
+            },
+            ["!Health"] = {
+              text_format = "[tx:health:percent:nosign] || [tx:health:current:shortvalue]",
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
+
+              font = F.FontOverride("- Steelfish"),
+              size = F.FontSizeScaled(22),
+              fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(32)),
+              yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
+            },
+            ["!Classification"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(0)),
+            },
+          },
+
+          raidRoleIcons = {
+            yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(10)),
+          },
+        },
+
+        pet = {
+          height = F.ChooseForTheme(F.Dpi(15), F.Dpi(20)),
+
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(0)),
+            },
+          },
+        },
+
+        targettarget = {
+          height = F.ChooseForTheme(F.Dpi(15), F.Dpi(20)),
+
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(0)),
+            },
+          },
+        },
+
+        focus = {
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(0)),
+            },
+            ["!Health"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(0)),
+
+              font = F.FontOverride("- Steelfish"),
+              size = F.FontSizeScaled(22),
+              fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(27)),
+              yOffset = 0,
+            },
+          },
+        },
+
+        party = {
+          customTexts = {
+            ["!Name"] = {
+              yOffset = F.Dpi(25),
+            },
+            ["!Health"] = {
+              yOffset = F.Dpi(25),
+
+              font = F.FontOverride("- Steelfish"),
+              size = F.FontSizeScaled(24),
+              fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
+            },
+            ["!ClassIcon"] = {
+              xOffset = F.Dpi(10),
+              yOffset = 0,
+            },
+          },
+
+          raidRoleIcons = {
+            yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(21)),
+          },
+        },
+
+        arena = {
+          customTexts = {
+            ["!Health"] = {
+              text_format = "[tx:health:percent:nosign] || [tx:health:current:shortvalue]",
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
+
+              font = F.FontOverride("- Steelfish"),
+              size = F.FontSizeScaled(20),
+              fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
+            },
+
+            ["!Name"] = {
+              yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
+            },
+          },
+        },
+
+        boss = {
+          customTexts = {
+            ["!Health"] = {
+              yOffset = F.Dpi(25),
+
+              font = F.FontOverride("- Steelfish"),
+              size = F.FontSizeScaled(24),
+              fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
+            },
+
+            ["!Name"] = {
+              yOffset = F.Dpi(-5),
+            },
+          },
         },
       },
+      F.Table.If(IsHealer, {
+        party = {
+          customTexts = {
+            ["!Health"] = {
+              yOffset = F.Dpi(0),
+            },
 
-      target = {
-        height = F.ChooseForTheme(F.Dpi(30), F.Dpi(40)),
-
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
-          },
-          ["!Health"] = {
-            text_format = "[tx:health:percent:nosign] || [tx:health:current:shortvalue]",
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
-
-            font = F.FontOverride("- Steelfish"),
-            size = F.FontSizeScaled(22),
-            fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
-          },
-          ["!ClassIcon"] = {
-            xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(32)),
-            yOffset = F.ChooseForTheme(F.Dpi(0), F.Dpi(-16)),
-          },
-          ["!Classification"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(0)),
+            ["!Name"] = {
+              yOffset = F.Dpi(0),
+            },
           },
         },
-
-        raidRoleIcons = {
-          yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(10)),
-        },
-      },
-
-      pet = {
-        height = F.ChooseForTheme(F.Dpi(15), F.Dpi(20)),
-
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(0)),
-          },
-        },
-      },
-
-      targettarget = {
-        height = F.ChooseForTheme(F.Dpi(15), F.Dpi(20)),
-
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(15), F.Dpi(0)),
-          },
-        },
-      },
-
-      focus = {
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(0)),
-          },
-          ["!Health"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(25), F.Dpi(0)),
-
-            font = F.FontOverride("- Steelfish"),
-            size = F.FontSizeScaled(22),
-            fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
-          },
-          ["!ClassIcon"] = {
-            xOffset = F.ChooseForTheme(F.Dpi(-5), F.Dpi(27)),
-            yOffset = 0,
-          },
-        },
-      },
-
-      party = {
-        customTexts = {
-          ["!Name"] = {
-            yOffset = F.Dpi(25),
-          },
-          ["!Health"] = {
-            yOffset = F.Dpi(25),
-
-            font = F.FontOverride("- Steelfish"),
-            size = F.FontSizeScaled(24),
-            fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
-          },
-          ["!ClassIcon"] = {
-            xOffset = F.Dpi(10),
-            yOffset = 0,
-          },
-        },
-
-        raidRoleIcons = {
-          yOffset = F.ChooseForTheme(F.Dpi(21), F.Dpi(21)),
-        },
-      },
-
-      arena = {
-        customTexts = {
-          ["!Health"] = {
-            text_format = "[tx:health:percent:nosign] || [tx:health:current:shortvalue]",
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
-
-            font = F.FontOverride("- Steelfish"),
-            size = F.FontSizeScaled(20),
-            fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
-          },
-
-          ["!Name"] = {
-            yOffset = F.ChooseForTheme(F.Dpi(27), F.Dpi(0)),
-          },
-        },
-      },
-
-      boss = {
-        customTexts = {
-          ["!Health"] = {
-            yOffset = F.Dpi(25),
-
-            font = F.FontOverride("- Steelfish"),
-            size = F.FontSizeScaled(24),
-            fontOutline = F.FontStyleOverride("- Steelfish", "SHADOW"),
-          },
-
-          ["!Name"] = {
-            yOffset = F.Dpi(-5),
-          },
-        },
-      },
-    })
+      })
+    )
   end
 
   local colors = PF:BuildColorsProfile()
