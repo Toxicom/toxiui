2c4989045716d9252a80a9eae47fdac2ada8968a
Tomas Leonavičius <69549795+Toxicom@users.noreply.github.com>
2023-12-26 00:13:55 +0200

    📦 NEW: Summon Yak on Durability RMB

--- Core/Changelog/6.5.2.lua
+++ Core/Changelog/6.5.2.lua
@@ -8,6 +8,12 @@ TXUI.Changelog["6.5.2"] = {
     "* New features",
     "Add option to toggle the ActionBars Fade visibility when in a Vehicle or DragonRiding"
       .. F.String.Sublist(F.String.FastGradientHex("Skins", "#ff77a9", "#b4004e") .. " -> " .. F.String.Class("ElvUI") .. " -> " .. F.String.Class("ActionBars Fade", "ROGUE")),
+    "Add Right Click option to WunderBar's " --
+      .. F.String.ToxiUI("Durability Module")
+      .. " to summon "
+      .. F.String.Class("Grand Expedition Yak")
+      .. F.String.Sublist("Retail only")
+      .. F.String.Sublist("Must have the mount learned"),
 
     "* Bug fixes",
 
