ae828b2f94f5be5f42df0c08d2102643b54665cf
Toxicom <tomas.98.leo@gmail.com>
2024-02-22 00:52:31 +0200

    🚀 RELEASE: 6.5.6

--- Core/Changelog/6.5.6.lua
+++ Core/Changelog/6.5.6.lua
@@ -3,17 +3,9 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.5.6"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
     "Prevent WunderBar erroring out on first initial load",
     "Add additional safety check for missing config info in Loadout names",
-
-    "* Profile updates",
-
-    "* Documentation",
 
     "* Settings refactoring",
     "Increase installer frame's scale for first-time installations",
