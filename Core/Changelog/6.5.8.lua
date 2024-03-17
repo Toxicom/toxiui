dc841fc13fe73ebc29eeaa858fbc5f0576b740ee
Toxicom <tomas.98.leo@gmail.com>
2024-03-18 00:09:48 +0200

    📖 DOC: update changelog

--- Core/Changelog/6.5.8.lua
+++ Core/Changelog/6.5.8.lua
@@ -4,6 +4,9 @@ TXUI.Changelog["6.5.8"] = {
   HOTFIX = true,
   CHANGES = {
     "* Breaking changes",
+    "Your selected Hearthstone in WunderBar is now randomized" --
+      .. F.String.Sublist("See " .. F.String.Class("New features", "MONK") .. " section")
+      .. F.String.Sublist("Can disable this in Hearthstone settings"),
 
     "* New features",
     "Option for random Hearthstone toy in WunderBar" --
