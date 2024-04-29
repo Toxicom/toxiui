289a0e674ecc6608579e6dad5c3cf2ba7651df1d
Toxicom <tomas.98.leo@gmail.com>
2024-04-29 11:34:33 +0300

    👌 IMPROVE: disable plater soft-interact icon

--- Core/Changelog/6.5.9.lua
+++ Core/Changelog/6.5.9.lua
@@ -33,6 +33,7 @@ TXUI.Changelog["6.5.9"] = {
     "Make Details windows wider" --
       .. F.String.Sublist("In some edge cases the name string would get too long to fit and cause all sorts of various bugs"),
     "Add " .. F.String.ToxiUI("Shadow Outline") .. " to most fonts",
+    "Disable soft-interact icon in Plater",
 
     "* Documentation",
     F.String.MinElv("13.61"),
