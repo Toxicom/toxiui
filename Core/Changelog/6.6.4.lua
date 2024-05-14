1467494654585dae2de829ae4354542788dbfcd2
Toxicom <tomas.98.leo@gmail.com>
2024-05-14 17:55:27 +0300

    🚀 RELEASE: 6.6.4

--- Core/Changelog/6.6.4.lua
+++ Core/Changelog/6.6.4.lua
@@ -3,21 +3,11 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.6.4"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
     "Fix Additional Addons installer images",
-
-    "* Profile updates",
-
-    "* Documentation",
 
     "* Settings refactoring",
     "Add page numbers to steps in the Installer",
     "Add more images to Installer",
-
-    "* Development improvements",
   },
 }
