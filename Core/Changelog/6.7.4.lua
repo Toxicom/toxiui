5f16f6483f2529aec6784304eee849f20e2cd3b2
Toxicom <tomas.98.leo@gmail.com>
2024-07-24 21:00:29 +0300

    🚀 RELEASE: 6.7.4

--- Core/Changelog/6.7.4.lua
+++ Core/Changelog/6.7.4.lua
@@ -3,20 +3,8 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.7.4"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
     "Fix DataBar for Cataclysm",
     "Fix Currency module's Shift-RightClick",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
