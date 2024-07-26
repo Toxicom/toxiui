6fa57b3b3649f6381b041534e2bf93f0317096c6
Toxicom <tomas.98.leo@gmail.com>
2024-07-26 08:56:54 +0300

    🚀 RELEASE: 6.7.5

--- Core/Changelog/6.7.5.lua
+++ Core/Changelog/6.7.5.lua
@@ -3,19 +3,14 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.7.5"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
     "Update skyriding logic",
     "Fix Hearthstone timer in " .. F.String.Menu.WunderBar(),
     "Fix Spellbook in " .. F.String.Menu.WunderBar() .. " MicroMenu",
     "Fix Talents in " .. F.String.Menu.WunderBar() .. " MicroMenu",
 
-    "* Profile updates",
-
     "* Documentation",
+    F.String.MinElv("13.72"),
 
     "* Settings refactoring",
     "Re-enable " .. F.String.ElvUI() .. " theme",
