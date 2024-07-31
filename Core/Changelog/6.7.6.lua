b1e8905ebcfb638a702268fe35bd3897937769d9
Toxicom <tomas.98.leo@gmail.com>
2024-07-31 11:31:09 +0300

    Revert "🚀 RELEASE: 6.7.6"
    This reverts commit a08693b689c5688762add892401361830ce69c75.

--- Core/Changelog/6.7.6.lua
+++ Core/Changelog/6.7.6.lua
@@ -3,6 +3,10 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.7.6"] = {
   HOTFIX = true,
   CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
     "* Bug fixes",
     "Fix AddOns not being recognized by " .. TXUI.Title .. " installer",
 
