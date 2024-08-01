22ad390d1738934e309b7463091440ca9adeb36c
Toxicom <tomas.98.leo@gmail.com>
2024-08-01 22:54:47 +0300

    📖 DOC: prep 6.7.7

---
+++ Core/Changelog/6.7.7.lua
@@ -0,0 +1,21 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+
+TXUI.Changelog["6.7.7"] = {
+  HOTFIX = true,
+  CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
+    "* Bug fixes",
+    "Again update Skyriding logic",
+
+    "* Profile updates",
+
+    "* Documentation",
+
+    "* Settings refactoring",
+
+    "* Development improvements",
+  },
+}
