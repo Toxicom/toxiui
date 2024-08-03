97d16a4e7796c66430d553169459cf23126b9014
Toxicom <tomas.98.leo@gmail.com>
2024-08-03 13:21:35 +0300

    🚀 RELEASE: 6.7.7

--- Core/Changelog/6.7.7.lua
+++ Core/Changelog/6.7.7.lua
@@ -3,16 +3,12 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.7.7"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     "Display Warbank money in Currency module for " .. F.String.Menu.WunderBar(),
 
     "* Bug fixes",
     "Again update Skyriding logic",
     "Fix Professions flyout tooltip not showing",
-
-    "* Profile updates",
 
     "* Documentation",
     "Add The Innkeeper's daughter hearthstone to Cataclysm",
@@ -22,7 +18,5 @@ TXUI.Changelog["6.7.7"] = {
     "Prefix Mythic+ portals with " .. F.String.Class("Mythic:", "DEMONHUNTER") .. " in " .. F.String.Menu.WunderBar() .. " Hearthstone select",
     "Sort Hearthstones alphabetically in " .. F.String.Menu.WunderBar() .. " Hearthstone select",
     "Fix incorrect option label in " .. F.String.Menu.WunderBar() .. " settings",
-
-    "* Development improvements",
   },
 }
