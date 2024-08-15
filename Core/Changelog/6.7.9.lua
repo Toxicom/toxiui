c7311505ceb53194f7bd437bc5b5f2b2af315ac8
Toxicom <tomas.98.leo@gmail.com>
2024-08-15 15:40:51 +0300

    📖 DOC: prepare 6.7.9

---
+++ Core/Changelog/6.7.9.lua
@@ -0,0 +1,20 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+
+TXUI.Changelog["6.7.9"] = {
+  HOTFIX = true,
+  CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
+    "* Bug fixes",
+
+    "* Profile updates",
+
+    "* Documentation",
+
+    "* Settings refactoring",
+
+    "* Development improvements",
+  },
+}
