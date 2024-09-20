9d23871fe3ce62337b39feddacbacacfde509d1a
Toxicom <tomas.98.leo@gmail.com>
2024-09-20 19:22:27 +0300

    🚀 RELEASE: 6.8.3

--- Core/Changelog/6.8.3.lua
+++ Core/Changelog/6.8.3.lua
@@ -3,8 +3,6 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.8.3"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     "Add Friends frame to " .. F.String.Scaling(),
 
@@ -12,13 +10,7 @@ TXUI.Changelog["6.8.3"] = {
     "Band-aid fix for " .. F.String.Menu.WunderBar() .. " Profession flyout's Herbalism icon freaking out",
     "Fix Dark Mode Gradient Names requirements erroring out",
 
-    "* Profile updates",
-
     "* Documentation",
     "Replace Wooly Mammoth with Traveler's Tundra Mammoth in " .. F.String.Menu.WunderBar() .. " Durability module",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
