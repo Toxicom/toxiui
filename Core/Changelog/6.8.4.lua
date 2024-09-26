f29dce3d3bb484d6979523fa86479a192c8f6e3a
Toxicom <tomas.98.leo@gmail.com>
2024-09-26 16:03:26 +0300

    📖 DOC: changelog

--- Core/Changelog/6.8.4.lua
+++ Core/Changelog/6.8.4.lua
@@ -6,6 +6,7 @@ TXUI.Changelog["6.8.4"] = {
     "* Breaking changes",
 
     "* New features",
+    F.String.Menu.WunderBar() .. " can now be positioned at the top >.>" .. F.String.Sublist("Credits to arturogutierrez"),
 
     "* Bug fixes",
     "Fix currency sorting issue where currencies would go under the wrong expansion header",
