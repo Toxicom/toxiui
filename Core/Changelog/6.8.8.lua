bbb29915438337fbb2cf3e02374c1504d8daada5
Toxicom <tomas.98.leo@gmail.com>
2024-11-19 22:55:03 +0200

    📖 DOC: increase min. req. elvui to 13.81

--- Core/Changelog/6.8.8.lua
+++ Core/Changelog/6.8.8.lua
@@ -8,17 +8,22 @@ TXUI.Changelog["6.8.8"] = {
     "* New features",
 
     "* Bug fixes",
-    "If " .. F.String.Menu.WunderBar() .. " is set to mouseover and you have a flyout open, it will no longer fade out until you close the flyout",
+    "If "
+      .. F.String.Menu.WunderBar()
+      .. " is set to mouseover and you have a flyout open, it will no longer fade out until you close the flyout",
     "Having a " .. F.String.Menu.WunderBar() .. " flyout open will close it when entering combat",
-    "Add a second Siege of Boralus M+ portal ID for different Horde & Alliance portals" .. F.String.Sublist("Credits to mdpaquin"),
+    "Add a second Siege of Boralus M+ portal ID for different Horde & Alliance portals"
+      .. F.String.Sublist("Credits to mdpaquin"),
     "Potential fix for AFK mode",
 
     "* Profile updates",
-    F.String.ElvUI() .. ": Change party/arena offset to use dynamic calculation" .. F.String.Sublist("This should revert the position to previous for non-ultrawide users"),
+    F.String.ElvUI() .. ": Change party/arena offset to use dynamic calculation" .. F.String.Sublist(
+      "This should revert the position to previous for non-ultrawide users"
+    ),
 
     "* Documentation",
     "Update for patch 11.0.5",
-    F.String.MinElv("13.80"),
+    F.String.MinElv("13.81"),
 
     "* Settings refactoring",
 
