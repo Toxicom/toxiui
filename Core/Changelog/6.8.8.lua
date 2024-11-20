f4e1d245f5c78b2a2f6f4d63d22886909dfe58e2
Toxicom <tomas.98.leo@gmail.com>
2024-11-20 23:36:01 +0200

    📖 DOC: 1.15.5 update

--- Core/Changelog/6.8.8.lua
+++ Core/Changelog/6.8.8.lua
@@ -11,13 +11,14 @@ TXUI.Changelog["6.8.8"] = {
     "If " .. F.String.Menu.WunderBar() .. " is set to mouseover and you have a flyout open, it will no longer fade out until you close the flyout",
     "Having a " .. F.String.Menu.WunderBar() .. " flyout open will close it when entering combat",
     "Add a second Siege of Boralus M+ portal ID for different Horde & Alliance portals" .. F.String.Sublist("Credits to mdpaquin"),
-    "Potential fix for AFK mode",
+    "Fix AFK mode not filling the whole screen",
 
     "* Profile updates",
     F.String.ElvUI() .. ": Change party/arena offset to use dynamic calculation" .. F.String.Sublist("This should revert the position to previous for non-ultrawide users"),
 
     "* Documentation",
     "Update for patch 11.0.5",
+    "Update for patch 1.15.5",
     F.String.MinElv("13.81"),
 
     "* Settings refactoring",
