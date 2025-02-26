114908f0b9e10afba6de9c825246d5659343bfbf
Toxicom <tomas.98.leo@gmail.com>
2025-02-26 11:54:47 +0200

    📖 DOC: bump toc

---
+++ Core/Changelog/6.9.1.lua
@@ -0,0 +1,22 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+
+TXUI.Changelog["6.9.1"] = {
+  HOTFIX = true,
+  CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
+    "* Bug fixes",
+
+    "* Profile updates",
+
+    "* Documentation",
+    "Update for Patch 11.0",
+    F.String.MinElv("13.85"),
+
+    "* Settings refactoring",
+
+    "* Development improvements",
+  },
+}
