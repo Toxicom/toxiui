d77713179e7e50cc4c9a5aa7b9f60866365a3816
Toxicom <tomas.98.leo@gmail.com>
2025-03-12 17:18:12 +0200

    👌 IMPROVE: remove plater Hide Nameplates script

--- Core/Changelog/6.9.1.lua
+++ Core/Changelog/6.9.1.lua
@@ -13,6 +13,7 @@ TXUI.Changelog["6.9.1"] = {
     "* Profile updates",
     TXUI.Title .. ": Enable Time module's resting animation by default",
     F.String.Plater() .. ": Add " .. F.String.Class("Black Arrow", "HUNTER") .. " to manual buff  tracking",
+    F.String.Plater() .. ": Remove Hide Nameplates script",
     F.String.ElvUI() .. ": Use the new absorb tag for " .. F.String.ToxiUI("toxiui:health-small") .. " custom texts",
     F.String.ElvUI() .. ": Reduce minimap location text font size",
 
