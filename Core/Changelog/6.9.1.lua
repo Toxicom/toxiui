a3ba1d092acfa837ba5de79b44cf582dddc83ebd
Toxicom <tomas.98.leo@gmail.com>
2025-03-13 10:20:52 +0200

    👌 IMPROVE: plater updates

--- Core/Changelog/6.9.1.lua
+++ Core/Changelog/6.9.1.lua
@@ -16,6 +16,9 @@ TXUI.Changelog["6.9.1"] = {
     F.String.Plater() .. ": Add " .. F.String.Class("Haunt", "WARLOCK") .. " to manual buff tracking",
     F.String.Plater() .. ": Remove Hide Nameplates script",
     F.String.Plater() .. ": Increase Aura Timer & Aura Stack font sizes",
+    F.String.Plater() .. ": Increase update interval to default 0.25" .. F.String.Sublist("Should increase performance with lots of nameplates on the screen"),
+    F.String.Plater() .. ": Update Blizzard nameplate fonts to " .. TXUI.Title,
+    F.String.Plater() .. ": Hide Blizzard Health Bars",
     F.String.ElvUI() .. ": Use the new absorb tag for " .. F.String.ToxiUI("toxiui:health-small") .. " custom texts",
     F.String.ElvUI() .. ": Reduce minimap location text font size",
 
