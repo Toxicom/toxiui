2a2dd2b3aa0e28b63f7c9854047dccd41f1fa05c
Toxicom <tomas.98.leo@gmail.com>
2025-03-17 01:08:44 +0200

    🚀 RELEASE: 6.9.3

--- Core/Changelog/6.9.3.lua
+++ Core/Changelog/6.9.3.lua
@@ -3,19 +3,7 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.9.3"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     F.String.Menu.Performance() .. " tab",
-
-    "* Bug fixes",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
