16230ea3ad399f3edc490ebf774e656419c783fc
Bill Riess <billriess86@gmail.com>
2025-04-12 02:17:46 -0400

    Update GameMenuSkin.lua
    Add new history limit slider

--- Modules/Options/Skins/GameMenuSkin.lua
+++ Modules/Options/Skins/GameMenuSkin.lua
@@ -17,7 +17,8 @@ function O:Skins_ElvUI()
     local mainGroup = self:AddInlineRequirementsDesc(options, {
       name = "Description",
     }, {
-      name = TXUI.Title .. " provides a skin for the Game Menu (ESC) that applies a background and additional information, all of which can be configured here.\n\n",
+      name = TXUI.Title ..
+          " provides a skin for the Game Menu (ESC) that applies a background and additional information, all of which can be configured here.\n\n",
     }, I.Requirements.GameMenuButton).args
 
     -- ToxiUI Game Menu Button Enable
@@ -44,9 +45,10 @@ function O:Skins_ElvUI()
     local backgroundGroup = self:AddInlineRequirementsDesc(options, {
       name = "Background",
     }, {
-      name = "Customize the color of the Game Menu Skin's background.\n\nThe " .. F.String.ToxiUI("Class Color") .. " option will override the " .. F.String.ToxiUI(
-        "Background Color"
-      ) .. " option, but you can still use it to control the alpha of the background.\n\n",
+      name = "Customize the color of the Game Menu Skin's background.\n\nThe " ..
+          F.String.ToxiUI("Class Color") .. " option will override the " .. F.String.ToxiUI(
+            "Background Color"
+          ) .. " option, but you can still use it to control the alpha of the background.\n\n",
     }, I.Requirements.GameMenuButton).args
 
     local optionsDisabled = function()
@@ -85,7 +87,8 @@ function O:Skins_ElvUI()
     local infoGroup = self:AddInlineRequirementsDesc(options, {
       name = "Information Sections",
     }, {
-      name = TXUI.Title .. " Game Menu Skin provides various useful information about your current session. You can choose what to display here.\n\n",
+      name = TXUI.Title ..
+          " Game Menu Skin provides various useful information about your current session. You can choose what to display here.\n\n",
     }, I.Requirements.GameMenuButton).args
 
     local optionsDisabled = function()
@@ -152,6 +155,24 @@ function O:Skins_ElvUI()
         E:StaticPopup_Show("CONFIG_RL")
       end,
       disabled = optionsDisabled,
+      hidden = not TXUI.IsRetail,
+    }
+
+    infoGroup.historyLimit = {
+      order = self:GetOrder(),
+      type = "range",
+      name = "History Limit",
+      desc = "Number of Mythic+ dungeons shown in the history list.",
+      min = 1,
+      max = 10,
+      step = 1,
+      get = function()
+        return E.db.TXUI.addons.gameMenuSkin.historyLimit
+      end,
+      set = function(_, value)
+        E.db.TXUI.addons.gameMenuSkin.historyLimit = value
+      end,
+      disabled = optionsDisabled and not E.db.TXUI.addons.gameMenuSkin.showMythic,
       hidden = not TXUI.IsRetail,
     }
   end
