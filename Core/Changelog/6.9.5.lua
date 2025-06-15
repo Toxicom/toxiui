2490e2e1354e5706c6c3726e780f51304bb822a9
Toxicom <tomas.98.leo@gmail.com>
2025-06-15 17:03:44 +0300

    👌 IMPROVE: add auras to plater manual tracking

--- Core/Changelog/6.9.5.lua
+++ Core/Changelog/6.9.5.lua
@@ -12,6 +12,13 @@ TXUI.Changelog["6.9.5"] = {
     "Update Pets collection text in Game Menu Skin when opening the menu",
 
     "* Profile updates",
+    F.String.Plater() --
+      .. ": Add these auras to manual tracking:"
+      .. F.String.Sublist(F.String.Class("Chains of Ice", "DEATHKNIGHT"))
+      .. F.String.Sublist(F.String.Class("Reaper's Mark", "DEATHKNIGHT"))
+      .. F.String.Sublist(F.String.Class("Sigil of Flame", "DEMONHUNTER"))
+      .. F.String.Sublist(F.String.Class("Fiery Brand", "DEMONHUNTER")),
+    F.String.Plater() .. ": Add " .. F.String.Class("Sigil of Silence", "DEMONHUNTER") .. " to Special Auras tracking",
 
     "* Documentation",
 
