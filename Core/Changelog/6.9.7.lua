9dbe58201fec569d7fd71b72bcaa66eb8bb43bfd
Toxicom <tomas.98.leo@gmail.com>
2025-07-02 23:18:46 +0300

    🚀 RELEASE: 6.9.7

--- Core/Changelog/6.9.7.lua
+++ Core/Changelog/6.9.7.lua
@@ -3,18 +3,7 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.9.7"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
+    "Fix Mists of Pandaria not being recognized by Curseforge or Wago",
   },
 }
