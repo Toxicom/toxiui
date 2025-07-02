4ebf916b1840f22c0562b8cf2093340d57f228b1
Toxicom <tomas.98.leo@gmail.com>
2025-07-02 22:23:17 +0300

    🚀 RELEASE: 6.9.6

--- Core/Changelog/6.9.6.lua
+++ Core/Changelog/6.9.6.lua
@@ -3,21 +3,15 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.9.6"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
     "Fix Plater import string for non-retail clients",
-
-    "* Profile updates",
+    "Various bug fixes for Mists of Pandaria",
 
     "* Documentation",
     F.String.MinElv("13.93"),
     "Update for Mists of Pandaria Classic Pre-Patch",
 
     "* Settings refactoring",
-
-    "* Development improvements",
+    "Add labels to Mage Portals in Classic Era & Mists of Pandaria Classic",
   },
 }
