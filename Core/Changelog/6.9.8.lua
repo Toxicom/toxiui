47293c5a6c11a4da6dfe7a67bd06bc664fbfca26
Toxicom <tomas.98.leo@gmail.com>
2025-07-15 16:07:21 +0300

    🚀 RELEASE: 6.9.8

--- Core/Changelog/6.9.8.lua
+++ Core/Changelog/6.9.8.lua
@@ -3,20 +3,8 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["6.9.8"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
     "Fix the '" .. TXUI.Title .. " White' Role Icons throwing an error",
     "Hide " .. F.String.Menu.WunderBar() .. " in Pet Battles for Mists of Pandaria Classic",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
