87408b81651ae3dea9cd5179b06cb9a7b4e013a0
Toxicom <tomas.98.leo@gmail.com>
2025-08-28 09:33:26 +0300

    🚀 RELEASE: 7.0.0

--- Core/Changelog/7.0.0.lua
+++ Core/Changelog/7.0.0.lua
@@ -3,21 +3,11 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.0.0"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Bug fixes",
     "Update " .. F.String.Menu.WunderBar() .. " Durability Module's logic for Mists of Pandaria Classic",
-
-    "* Profile updates",
 
     "* Documentation",
     "Update " .. F.String.Menu.WunderBar() .. " Currency Module's currencies for TWW S3",
     "Add Vale of Eternal Blossoms mage portals to " .. F.String.Menu.WunderBar() .. " in Mists of Pandaria Classic",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
