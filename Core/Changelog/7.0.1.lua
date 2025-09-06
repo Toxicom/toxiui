8a7ffc21112dd753d3f74c668b0c913bda8d1a9c
Toxicom <tomas.98.leo@gmail.com>
2025-09-06 11:13:23 +0300

    👌 IMPROVE: update plater colors

--- Core/Changelog/7.0.1.lua
+++ Core/Changelog/7.0.1.lua
@@ -10,6 +10,7 @@ TXUI.Changelog["7.0.1"] = {
     "* Bug fixes",
 
     "* Profile updates",
+    F.String.Plater() .. ": Update npc colors for season 3",
 
     "* Documentation",
     "Add Cosmic Hearthstone to " .. F.String.Menu.WunderBar(),
