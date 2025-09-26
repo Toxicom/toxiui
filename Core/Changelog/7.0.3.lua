37ab08218488bd14ac5e0a85c35f766b5ba78dd5
Toxicom <tleonavic@gmail.com>
2025-09-26 17:03:53 +0300

    🚀 RELEASE: 7.0.3

--- Core/Changelog/7.0.3.lua
+++ Core/Changelog/7.0.3.lua
@@ -3,12 +3,8 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.0.3"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     "Add options to toggle Keybind & Macro texts on Vehicle Bar buttons",
-
-    "* Bug fixes",
 
     "* Profile updates",
     "Update ElvUI filter names to account for 14.00 changes",
@@ -18,9 +14,5 @@ TXUI.Changelog["7.0.3"] = {
     "* Documentation",
     "Update .toc for Mists of Pandaria Classic",
     F.String.MinElv("14.00"),
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
