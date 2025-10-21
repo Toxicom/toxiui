990a35edaafb48c9eb82d3c1e12e4fc254844be8
Toxicom <tleonavic@gmail.com>
2025-10-21 22:32:30 +0300

    🚀 RELEASE: 7.0.5

--- Core/Changelog/7.0.5.lua
+++ Core/Changelog/7.0.5.lua
@@ -3,21 +3,9 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.0.5"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Bug fixes",
-
     "* Profile updates",
     F.String.Plater() .. ": Add Legion Remix Empowered buffs to both Automatic and Manual tracking",
     F.String.Plater() .. ": Add " .. F.String.ToxiUI("Force Threat Colors") .. " mod to override M+ colors when you have aggro",
     F.String.Plater() .. ": Re-add " .. F.String.Class("Sigil of Silence", "DEMONHUNTER") .. " to Buff Special",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
