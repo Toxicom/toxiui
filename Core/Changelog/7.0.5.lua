4dcbc271d5d287eaa1e8818cdd36358b045482a7
Toxicom <tleonavic@gmail.com>
2025-10-21 21:28:06 +0300

    🐛 FIX: plater threat colors

--- Core/Changelog/7.0.5.lua
+++ Core/Changelog/7.0.5.lua
@@ -11,6 +11,8 @@ TXUI.Changelog["7.0.5"] = {
 
     "* Profile updates",
     F.String.Plater() .. ": Add Legion Remix Empowered buffs to both Automatic and Manual tracking",
+    F.String.Plater() .. ": Add " .. F.String.ToxiUI("Force Threat Colors") .. " mod to override M+ colors when you have aggro",
+    F.String.Plater() .. ": Re-add " .. F.String.Class("Sigil of Silence", "DEMONHUNTER") .. " to Buff Special",
 
     "* Documentation",
 
