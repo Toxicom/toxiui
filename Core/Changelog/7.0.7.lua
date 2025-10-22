37971e95dcba2c59c776094062cfa3aa20baf621
Toxicom <tomas.98.leo@gmail.com>
2025-10-22 10:15:18 +0300

    📖 DOC: prep 7.0.7

---
+++ Core/Changelog/7.0.7.lua
@@ -0,0 +1,21 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+
+TXUI.Changelog["7.0.7"] = {
+  HOTFIX = true,
+  CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
+    "* Bug fixes",
+
+    "* Profile updates",
+
+    "* Documentation",
+    "Update .toc for Classic Era patch 1.15.8",
+
+    "* Settings refactoring",
+
+    "* Development improvements",
+  },
+}
