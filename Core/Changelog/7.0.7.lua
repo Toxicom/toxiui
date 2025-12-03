59c0617171129d9544abf93612428f2363ea5274
Toxicom <tomas.98.leo@gmail.com>
2025-12-03 15:49:58 +0200

    📖 DOC: 11.2.7 toc

--- Core/Changelog/7.0.7.lua
+++ Core/Changelog/7.0.7.lua
@@ -15,6 +15,7 @@ TXUI.Changelog["7.0.7"] = {
     "* Documentation",
     "Update .toc for Classic Era patch 1.15.8",
     "Update .toc for Mists of Pandaria patch 5.5.2",
+    "Update .toc for The War Within patch 11.2.7",
     F.String.MinElv("14.03"),
 
     "* Settings refactoring",
