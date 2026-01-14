3d66b8a29240553e5c8a006e453af0d2b6ec5b93
Toxicom <tomas.98.leo@gmail.com>
2026-01-14 16:01:07 +0200

    🚀 RELEASE: 7.1.0

--- Core/Changelog/7.1.0.lua
+++ Core/Changelog/7.1.0.lua
@@ -3,22 +3,12 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.1.0"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     "Support for The Burning Crusade Anniversary" --
       .. F.String.Sublist("Credits to roo7cause for initial PR")
       .. F.String.Sublist("Most fixes are just aligning TBC code with Vanilla code"),
 
-    "* Bug fixes",
-
-    "* Profile updates",
-
     "* Documentation",
     F.String.MinElv("14.06"),
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
