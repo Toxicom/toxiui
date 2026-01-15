a7ccd95ec95ee32f168c014d2fddcb14f9047d3f
Toxicom <tomas.98.leo@gmail.com>
2026-01-15 11:33:23 +0200

    🚀 RELEASE: 7.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,1 +1,14 @@
-# ToxiUI v7.1.1 - 2026-01-14
+# ToxiUI v7.1.1 - 2026-01-15
+
+## ‼️ Breaking Changes
+
+- **ToxiUI ElvUI Skin Theme** is now disabled by default
+
+## 🐛 Bug Fixes
+
+- Improve initialization performance by deferring non-critical module setups
+  - This code was always applied for Retail; it is now applied for all versions.
+
+## 🧩 Profile Updates
+
+- Move **WTRaidMarkersBarAnchor** mover position when Chattynator is enabled
--- Core/Changelog/7.1.1.lua
+++ Core/Changelog/7.1.1.lua
@@ -6,19 +6,11 @@ TXUI.Changelog["7.1.1"] = {
     "* Breaking changes",
     TXUI.Title .. " " .. F.String.ElvUI() .. " Skin Theme disabled by default",
 
-    "* New features",
-
     "* Bug fixes",
     "Improve initialization performance by deferring non-critical module setups."
       .. F.String.Sublist("This code was always applied for Retail, now it's applied for all versions."),
 
     "* Profile updates",
     "Move WTRaidMarkersBarAnchor mover position when Chattynator is enabled.",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
