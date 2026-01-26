a1e42a6a90c84a6e5c2daad902f8324cc32f67b7
Toxicom <tomas.98.leo@gmail.com>
2026-01-26 17:14:12 +0200

    🚀 RELEASE: 7.2.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,12 @@
 # ToxiUI v7.2.1 - 2026-01-26
 
+## 🚀 New Features
+
+- Return **ToxiUI WeakAura Anchor** for non-Retail versions
+
 ## 🧩 Profile Updates
 
+- **ToxiUI**: Use 3:2 aspect ratio for Vehicle Bar buttons
+- **ToxiUI**: Display Soul Shards for `tx:power` tag on Retail
 - **ElvUI**: Fix position of Class Bar for Retail
+- **ElvUI**: Use raw power tag instead of percentage for the player power bar
--- Core/Changelog/7.2.1.lua
+++ Core/Changelog/7.2.1.lua
@@ -3,23 +3,13 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.2.1"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     "Return ToxiUI WA Anchor for non-Retail versions",
-
-    "* Bug fixes",
 
     "* Profile updates",
     TXUI.Title .. ": Use 3:2 aspect ratio for Vehicle Bar buttons",
     TXUI.Title .. ": Display soul shards for tx:power tag on Retail",
     F.String.ElvUI() .. ": Fix position of Class Bar for Retail",
     F.String.ElvUI() .. ": Use raw power tag instead of percentage for player power bar",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
