7ebedc3903318fecb593225bced1a1ddcaf027c2
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-02-11 22:58:25 +0200

    🚀 RELEASE: 7.2.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,17 @@
 # ToxiUI v7.2.7 - 2026-02-11
 
+## 🚀 New Features
+
+- [Retail] **ToxiUI Cooldown Manager Skin** Keybinds
+
+## ✨ Enhancements
+
+- [Retail] Disable **ToxiUI Cooldown Manager Skin** by default
+
 ## 🐛 Bug Fixes
 
-- [Retail] Disable CDM Castbar sync by default
+- [Retail] Do not anchor **ToxiUI Cooldown Manager Skin** essentials when power bar is disabled
+
+## 🔧 Development Improvements
+
+- Rename all internal cases of referencing game versions
--- Core/Changelog/7.2.7.lua
+++ Core/Changelog/7.2.7.lua
@@ -3,21 +3,14 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.2.7"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
+    F.String.Retail() .. F.String.CDM() .. " Keybinds",
 
     "* Enhancements",
     F.String.Retail() .. "Disable " .. F.String.CDM() .. " by default",
 
     "* Bug fixes",
     F.String.Retail() .. "Do not anchor " .. F.String.CDM() .. " essentials when power bar is disabled",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
 
     "* Development improvements",
     "Rename all internal cases of referencing game versions",
