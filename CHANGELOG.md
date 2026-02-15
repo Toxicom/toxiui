644672886db42fd11caddde04fef0fc9175eec77
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-02-15 18:26:01 +0200

    🚀 RELEASE: 7.2.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,23 @@
-# ToxiUI v7.2.9-beta.1 - 2026-02-13
+# ToxiUI v7.2.9 - 2026-02-15
 
 ## 🚀 New Features
 
 - **ToxiUI** Profile Updater
   - /tx update; /tx u
+- Allow changing fade direction for Gradient mode
+
+## ✨ Enhancements
+
+- [Retail] Decouple Damage Meter Skin from **ElvUI**
+  - You can now enjoy default blizzard damage meter with ToxiUI icons and gradients
+- [Retail] Disable **ToxiUI Cooldown Manager Skin** if ArcUI is enabled
+- [Retail] Improve **ToxiUI Cooldown Manager Skin** Anchoring logic
+
+## 🐛 Bug Fixes
+
+- [Retail] Fix disappearing gradients in dungeons for Damage Meter Skin
+
+## 🧩 Profile Updates
+
+- **ElvUI**: Enable environment conditions for Nameplates
+- [Retail] **WindTools**: Enable Objective Tracker skin
--- Core/Changelog/7.2.9.lua
+++ Core/Changelog/7.2.9.lua
@@ -3,8 +3,6 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.2.9"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     TXUI.Title .. " Profile Updater" .. F.String.Sublist("/tx update; /tx u"),
     "Allow changing fade direction for " .. F.String.GradientString() .. " mode",
@@ -14,6 +12,7 @@ TXUI.Changelog["7.2.9"] = {
       "You can now enjoy default blizzard damage meter with ToxiUI icons and gradients"
     ),
     F.String.Retail() .. "Disable " .. F.String.CDM() .. " if ArcUI is enabled",
+    F.String.Retail() .. "Improve " .. F.String.CDM() .. " Anchoring logic",
 
     "* Bug fixes",
     F.String.Retail() .. "Fix disappearing gradients in dungeons for Damage Meter Skin",
@@ -21,11 +20,5 @@ TXUI.Changelog["7.2.9"] = {
     "* Profile updates",
     F.String.ElvUI() .. ": Enable environment conditions for Nameplates",
     F.String.Retail() .. F.String.WindTools() .. ": Enable Objective Tracker skin",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
