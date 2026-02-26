4f7bcfac1b6fff715e42267e480555ee86b68b85
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-02-26 17:58:56 +0200

    🐛 FIX: party/raid fade direction

---
+++ Core/Changelog/7.3.3.lua
@@ -0,0 +1,23 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+
+TXUI.Changelog["7.3.3"] = {
+  HOTFIX = true,
+  CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
+    "* Enhancements",
+
+    "* Bug fixes",
+    "Fix incorrect gradient fade direction for party and raid frames",
+
+    "* Profile updates",
+
+    "* Documentation",
+
+    "* Settings refactoring",
+
+    "* Development improvements",
+  },
+}
