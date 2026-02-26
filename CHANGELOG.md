96773770aa6569d0158372e0e3b2cd80dea724ca
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-02-27 00:04:37 +0200

    🚀 RELEASE: 7.3.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
-# ToxiUI v7.3.3 - 2026-02-26
+# ToxiUI v7.3.3 - 2026-02-27
 
 **Full Changelog**: https://github.com/Toxicom/toxiui/compare/v7.3.2...v7.3.3
+
+## 🐛 Bug Fixes
+
+- Fix incorrect gradient fade direction for party and raid frames
+- Display labels for additional hearthstone flyout
--- Core/Changelog/7.3.3.lua
+++ Core/Changelog/7.3.3.lua
@@ -3,22 +3,8 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 TXUI.Changelog["7.3.3"] = {
   HOTFIX = true,
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Enhancements",
-
     "* Bug fixes",
     "Fix incorrect gradient fade direction for party and raid frames",
     "Display labels for additional hearthstone flyout",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
