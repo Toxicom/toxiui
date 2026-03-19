16cadec46c4c7f5afcb8326f1abd64716967e09a
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-03-19 16:23:29 +0200

    docs: prepare 7.3.8

---
+++ Core/Changelog/7.3.8.lua
@@ -0,0 +1,23 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+
+TXUI.Changelog["7.3.8"] = {
+  HOTFIX = true,
+  RELEASE_DATE = "",
+  CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
+    "* Enhancements",
+
+    "* Bug fixes",
+
+    "* Profile updates",
+
+    "* Documentation",
+
+    "* Settings refactoring",
+
+    "* Development improvements",
+  },
+}
