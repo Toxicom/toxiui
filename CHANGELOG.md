8dc3acc711b2d250b667e6678fce5c77748a7f5f
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-03-19 18:49:03 +0200

    release: 7.3.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
 # ToxiUI v7.3.8 - 2026-03-19
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.3.7...v7.3.8>
+
+## 🐛 Bug Fixes
+
+- [Retail] **ToxiUI Cooldown Manager Skin**: Disable dynamic castbar width by default
+  - Got enabled by accident
+- [Retail] **ToxiUI Cooldown Manager Skin**: Remove buff bar dynamic width option
+  - Taint city, not worth it
--- Core/Changelog/7.3.8.lua
+++ Core/Changelog/7.3.8.lua
@@ -2,24 +2,10 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.3.8"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Mar 19, 2026",
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Enhancements",
-
     "* Bug fixes",
     F.String.Retail() .. F.String.CDM() .. ": Disable dynamic castbar width by default" .. F.String.Sublist("Got enabled by accident"),
     F.String.Retail() .. F.String.CDM() .. ": Remove buff bar dynamic width option" .. F.String.Sublist("Taint city, not worth it"),
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
