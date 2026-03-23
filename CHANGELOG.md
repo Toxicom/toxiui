5e0dd06308783a32726b2e5caed389a59c079116
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-03-23 12:01:19 +0200

    release: 7.3.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,21 @@
-# ToxiUI v7.3.9 - 2026-03-21
+# ToxiUI v7.3.9 - 2026-03-23
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.3.8...v7.3.9>
+
+## 🚀 New Features
+
+- **ToxiUI** Landing pages
+  - Will show a landing page on fresh install for some instructions
+  - Will show a landing page on addon update to remind about the Profile Updater feature
+
+## ✨ Enhancements
+
+- [Retail] **ToxiUI Cooldown Manager Skin**: Round up bar width to potentially have better pixel perfect alignment
+
+## 🧩 Profile Updates
+
+- **ElvUI** Unitframes: Update raid frames design
+
+## 📘 Documentation
+
+- [Retail] Armory: Adjust which slots require enchants/sockets for Midnight
--- Core/Changelog/7.3.9.lua
+++ Core/Changelog/7.3.9.lua
@@ -2,29 +2,20 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.3.9"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Mar 23, 2026",
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     TXUI.Title .. " Landing pages" .. F.String.Sublist("Will show a landing page on fresh install for some instructions") .. F.String.Sublist(
       "Will show a landing page on addon update to remind about the Profile Updater feature"
     ),
 
     "* Enhancements",
-    F.String.Retail() .. "Adjust which armory slots require enchants/sockets for Midnight",
     F.String.Retail() .. F.String.CDM() .. ": Round up bar width to potentially have better pixel perfect alignment",
-
-    "* Bug fixes",
 
     "* Profile updates",
     F.String.ElvUI() .. " Unitframes: Update raid frames design",
 
     "* Documentation",
-    F.String.Retail() .. " Armory: Add missing socket warnings to helm/wrist/belt",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
+    F.String.Retail() .. "Armory: Adjust which slots require enchants/sockets for Midnight",
   },
 }
