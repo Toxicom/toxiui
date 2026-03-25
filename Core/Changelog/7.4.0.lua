0bbb2ba6cadb90986444f0e381da9fa1ce1ae133
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-03-25 11:33:48 +0200

    docs: elvui 15.10

--- Core/Changelog/7.4.0.lua
+++ Core/Changelog/7.4.0.lua
@@ -18,6 +18,7 @@ TXUI.Changelog["7.4.0"] = {
     "Profile Updater: Display color differences as a single HEX unit instead of separate R G B units",
     "Profile Updater: Add open & close animations",
     F.String.Retail() .. F.String.CDM() .. ": Slightly stabler code due to using built-in Blizzard API instead of self made stuff",
+    F.String.Retail() .. F.String.CDM() .. ": Update keybind frame level to avoid situations where it's behind power bar",
     "Gradient Mode: Cache some function closures for potentially better performance",
     "Landing Page: Show profile updater button in update landing page",
     "Landing Page: Display images for both landing pages",
@@ -33,11 +34,11 @@ TXUI.Changelog["7.4.0"] = {
     F.String.ElvUI() .. " Unitframes: Re-position and re-size private auras for raid frames",
     F.String.ElvUI() .. " Unitframes: Re-position buffs and role icon for party frames",
     F.String.ElvUI() .. " Action Bars: Adjust buttons per row and button spacing",
-    F.String.Retail() .. F.String.CDM() .. ": Update keybind frame level to avoid situations where it's behind power bar",
     F.String.WindTools() .. ": Change Damage Meter texture to KMT47",
     F.String.WindTools() .. ": Setup Damage Meter fading options",
 
     "* Documentation",
+    F.String.MinElv("15.10"),
 
     "* Settings refactoring",
 
