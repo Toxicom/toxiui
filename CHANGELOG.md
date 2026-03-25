38a521627df54638a7787c74b81c3a78b77b1623
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-03-25 20:08:44 +0200

    release: 7.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 # ToxiUI v7.4.1 - 2026-03-25
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.4.0...v7.4.1>
+
+## 🐛 Bug Fixes
+
+- [Retail] Cooldown Manager Keybinds: Lower strata to avoid overlap
+- [Retail] Cooldown Manager Centering: Buff bar width collapsing
+- Gradient Mode: Wrong colors on group unitframes
--- Core/Changelog/7.4.1.lua
+++ Core/Changelog/7.4.1.lua
@@ -2,25 +2,11 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.4.1"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Mar 25, 2026",
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Enhancements",
-
     "* Bug fixes",
     F.String.Retail() .. F.String.CDM() .. " Keybinds: Lower strata to avoid overlap",
     F.String.Retail() .. F.String.CDM() .. " Centering: Buff bar width collapsing",
     F.String.GradientMode() .. ": Wrong colors on group unitframes",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
