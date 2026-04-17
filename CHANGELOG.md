fb3a4d23f8234997f5ce438b3601e1e55f56051f
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-04-18 02:00:43 +0300

    release: 7.4.3

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,13 @@
-# ToxiUI v7.4.3 - 2026-04-08
+# ToxiUI v7.4.3 - 2026-04-18
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.4.2...v7.4.3>
+
+## ✨ Enhancements
+
+- Vehicle Bar buttons will respect ElvUI's Pick Up Action Key
+  - SHIFT by default
+- [Retail] Cooldown Manager: Remove dynamic adjustment of class bar spacing
+
+## 🧩 Profile Updates
+
+- ElvUI Unitframes: Update leader icon position for horizontal party unitframes
--- Core/Changelog/7.4.3.lua
+++ Core/Changelog/7.4.3.lua
@@ -2,25 +2,13 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.4.3"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Apr 18, 2026",
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
     "* Enhancements",
     "Vehicle Bar buttons will respect " .. F.String.ElvUI() .. "'s Pick Up Action Key" .. F.String.Sublist("SHIFT by default"),
     F.String.Retail() .. F.String.CDM() .. ": Remove dynamic adjustment of class bar spacing",
 
-    "* Bug fixes",
-
     "* Profile updates",
     F.String.ElvUI() .. " Unitframes: Update leader icon position for horizontal party unitframes",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
