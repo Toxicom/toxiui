a539ea5ba945b2935f49631ff04bf0743b7aab50
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-05-03 22:00:52 +0300

    docs: changelog

--- Core/Changelog/7.4.5.lua
+++ Core/Changelog/7.4.5.lua
@@ -2,14 +2,10 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.4.5"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "May 03, 2026",
   CHANGES = {
     "* Breaking changes",
     "Remove " .. F.String.Details() .. " Gradient and Dark mode skinning",
-
-    "* New features",
-
-    "* Enhancements",
 
     "* Bug fixes",
     F.String.Retail() .. "UnitSpeed is now secret in combat - do not display speed in vigor bar during combat",
@@ -22,9 +18,5 @@ TXUI.Changelog["7.4.5"] = {
 
     "* Documentation",
     F.String.MinElv("15.13"),
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
