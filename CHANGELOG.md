0212f8d979a46d3b4750eff44bda3ee654a6ad62
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-05-15 12:28:08 +0300

    release: 7.4.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 # ToxiUI v7.4.6 - 2026-05-15
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.4.5...v7.4.6>
+
+## 🐛 Bug Fixes
+
+- [Retail] Damage Meter: Fix the 'Hide Floating Player Entry' option
+- [Retail] Armory: Safeguard stats in combat
+  - Funky fix, might still break
--- Core/Changelog/7.4.6.lua
+++ Core/Changelog/7.4.6.lua
@@ -2,24 +2,10 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.4.6"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "May 15, 2026",
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Enhancements",
-
     "* Bug fixes",
     F.String.Retail() .. "Damage Meter: Fix the 'Hide Floating Player Entry' option",
     F.String.Retail() .. "Armory: Safeguard stats in combat" .. F.String.Sublist("Funky fix, might still break"),
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
