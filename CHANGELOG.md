7938642737cfe4912945fb49cc680dadf5afaf8a
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-06-19 22:04:49 +0300

    release: 7.4.8

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
-# ToxiUI v7.4.8 - 2026-06-06
+# ToxiUI v7.4.8 - 2026-06-19
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.4.7...v7.4.8>
+
+## 📘 Documentation
+
+- [Retail] Update for 12.0.7
+- Update Liue and v0dKa names
+- Increase minimum required **ElvUI** version to `15.17`
--- Core/Changelog/7.4.8.lua
+++ Core/Changelog/7.4.8.lua
@@ -2,25 +2,11 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.4.8"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Jun 19, 2026",
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Enhancements",
-
-    "* Bug fixes",
-
-    "* Profile updates",
-
     "* Documentation",
     F.String.Retail() .. "Update for 12.0.7",
     "Update Liue and v0dKa names",
     F.String.MinElv("15.17"),
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
