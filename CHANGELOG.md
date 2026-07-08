d9a454a1600410488d3a15531db4c1e51a3bfff5
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-07-09 00:44:41 +0300

    docs: prepare 7.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,9 +1,3 @@
-# ToxiUI v7.4.9 - 2026-07-09
+# ToxiUI v7.5.0 - 2026-07-09
 
-**Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.4.8...v7.4.9>
-
-## 📘 Documentation
-
-- Add BehkFox to contributors
-- Update for TBC 2.5.6
-- Increase minimum required **ElvUI** version to `15.18`
+**Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.4.9...v7.5.0>
---
+++ Core/Changelog/7.5.0.lua
@@ -0,0 +1,23 @@
+local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
+
+TXUI.Changelog["7.5.0"] = {
+  HOTFIX = true,
+  RELEASE_DATE = "",
+  CHANGES = {
+    "* Breaking changes",
+
+    "* New features",
+
+    "* Enhancements",
+
+    "* Bug fixes",
+
+    "* Profile updates",
+
+    "* Documentation",
+
+    "* Settings refactoring",
+
+    "* Development improvements",
+  },
+}
