2ce5aa619bb1a5e0d81cffbb30da8395d39d8b68
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-08-13 22:16:02 +0300

    release: 7.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,24 @@
-# ToxiUI v7.5.0 - 2026-07-09
+# ToxiUI v7.5.0 - 2026-08-13
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.4.9...v7.5.0>
+
+## 🚀 New Features
+
+- Add WunderBar Durability module's middle-click to summon an Auction House mount
+  - Credits to bifferz
+
+## 🐛 Bug Fixes
+
+- Replace old usage of E:UpdateAll()
+- Fix WunderBar DataBar's max level check erroring after a recent API change
+  - Falls back to the old API on Classic/Anniversary where the new one isn't available
+
+## 📘 Documentation
+
+- Increase minimum required **ElvUI** version to `15.21`
+
+## 🔧 Development Improvements
+
+- Fix the `/tx dev export names` command
+  - Rewrite to use CBOR encoding instead of AceSerializer/LibDeflate
+  - Credits to Tibs
--- Core/Changelog/7.5.0.lua
+++ Core/Changelog/7.5.0.lua
@@ -2,14 +2,10 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.5.0"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Aug 13, 2026",
   CHANGES = {
-    "* Breaking changes",
-
     "* New features",
     "Add " .. F.String.Menu.WunderBar() .. " Durability module's middle-click to summon an Auction House mount" .. F.String.Sublist("Credits to bifferz"),
-
-    "* Enhancements",
 
     "* Bug fixes",
     "Replace old usage of E:UpdateAll()",
@@ -17,12 +13,8 @@ TXUI.Changelog["7.5.0"] = {
       "Falls back to the old API on Classic/Anniversary where the new one isn't available"
     ),
 
-    "* Profile updates",
-
     "* Documentation",
     F.String.MinElv("15.21"),
-
-    "* Settings refactoring",
 
     "* Development improvements",
     "Fix the `/tx dev export names` command" .. F.String.Sublist("Rewrite to use CBOR encoding instead of AceSerializer/LibDeflate") .. F.String.Sublist("Credits to Tibs"),
