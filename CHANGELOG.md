687bab2fa0f6b8be37cd42766dcfb357145b18ba
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-08-13 22:42:20 +0300

    release: 7.5.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 # ToxiUI v7.5.1 - 2026-08-13
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.5.0...v7.5.1>
+
+## 🐛 Bug Fixes
+
+- Even more secret protection for gradients
--- Core/Changelog/7.5.1.lua
+++ Core/Changelog/7.5.1.lua
@@ -2,23 +2,9 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.5.1"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Aug 13, 2026",
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Enhancements",
-
     "* Bug fixes",
     "Even more secret protection for gradients",
-
-    "* Profile updates",
-
-    "* Documentation",
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
