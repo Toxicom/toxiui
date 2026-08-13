1b476d80c52f99e51be63405c2f980d70684ddff
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-08-13 22:14:47 +0300

    docs: update changelog

--- Core/Changelog/7.5.0.lua
+++ Core/Changelog/7.5.0.lua
@@ -7,11 +7,15 @@ TXUI.Changelog["7.5.0"] = {
     "* Breaking changes",
 
     "* New features",
+    "Add " .. F.String.Menu.WunderBar() .. " Durability module's middle-click to summon an Auction House mount" .. F.String.Sublist("Credits to bifferz"),
 
     "* Enhancements",
 
     "* Bug fixes",
     "Replace old usage of E:UpdateAll()",
+    "Fix " .. F.String.Menu.WunderBar() .. " DataBar's max level check erroring after a recent API change" .. F.String.Sublist(
+      "Falls back to the old API on Classic/Anniversary where the new one isn't available"
+    ),
 
     "* Profile updates",
 
@@ -21,5 +25,6 @@ TXUI.Changelog["7.5.0"] = {
     "* Settings refactoring",
 
     "* Development improvements",
+    "Fix the `/tx dev export names` command" .. F.String.Sublist("Rewrite to use CBOR encoding instead of AceSerializer/LibDeflate") .. F.String.Sublist("Credits to Tibs"),
   },
 }
