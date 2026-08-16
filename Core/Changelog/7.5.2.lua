0b3a46337767124e68a15e511757b582a7e26f40
Jake Roper <jakeropergit@outlook.com>
2026-08-16 18:22:17 +0100

    Documentation

--- Core/Changelog/7.5.2.lua
+++ Core/Changelog/7.5.2.lua
@@ -11,12 +11,13 @@ TXUI.Changelog["7.5.2"] = {
     "* Enhancements",
 
     "* Bug fixes",
+    F.String.Retail() .. "Fixed error being raised on class icon tag due to secrets.",
 
     "* Profile updates",
 
     "* Documentation",
     F.String.MinElv("15.22"),
-    F.String.Retail() ..  "Update M+ S2 portals for " .. F.String.Menu.WunderBar(),
+    F.String.Retail() .. "Update M+ S2 portals for " .. F.String.Menu.WunderBar(),
 
     "* Settings refactoring",
 
