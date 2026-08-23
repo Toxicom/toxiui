66ba135b557ce01790ed85a2f5cd2fabe291c9dd
Toxicom <69549795+Toxicom@users.noreply.github.com>
2026-08-23 10:14:11 +0300

    release: 7.5.4

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,11 @@
-# ToxiUI v7.5.4 - 2026-08-21
+# ToxiUI v7.5.4 - 2026-08-23
 
 **Full Changelog**: <https://github.com/Toxicom/toxiui/compare/v7.5.3...v7.5.4>
+
+## 🐛 Bug Fixes
+
+- [Retail] Potentially fix Armory's nil string error
+
+## 📘 Documentation
+
+- Increase minimum required **ElvUI** version to `15.25`
--- Core/Changelog/7.5.4.lua
+++ Core/Changelog/7.5.4.lua
@@ -2,24 +2,12 @@ local TXUI, F, E, I, V, P, G = unpack((select(2, ...)))
 
 TXUI.Changelog["7.5.4"] = {
   HOTFIX = true,
-  RELEASE_DATE = "",
+  RELEASE_DATE = "Aug 23, 2026",
   CHANGES = {
-    "* Breaking changes",
-
-    "* New features",
-
-    "* Enhancements",
-
     "* Bug fixes",
     F.String.Retail() .. "Potentially fix Armory's nil string error",
 
-    "* Profile updates",
-
     "* Documentation",
     F.String.MinElv("15.25"),
-
-    "* Settings refactoring",
-
-    "* Development improvements",
   },
 }
